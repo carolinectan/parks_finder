312eeebf14ee961c92635d4a2c8f4c4301cea032
Caroline Tan <carolinectan3@gmail.com>
2021-09-07 01:21:44 -0400

    Refactor hours of operation

--- app/poros/park.rb
+++ app/poros/park.rb
@@ -6,7 +6,22 @@ class Park
     @name = hash[:fullName]
     @description = hash[:description]
     @directions = hash[:directionsInfo]
-    # require "pry"; binding.pry
-    @hours = hash[:operatingHours].first[:standardHours] # => i.e. {day: "times", etc.}
+    @hours = hours_by_day(hash)
+
+    #
+    # .map do |day|
+    #   require "pry"; binding.pry
+    #   day[0].to_s.capitalize + ": " + day[1]
+    # end.join(", ")
+  end
+
+  def hours_by_day(hash)
+    "Sunday: #{hash[:operatingHours][0][:standardHours][:sunday]}, " +
+    "Monday: #{hash[:operatingHours][0][:standardHours][:monday]}, " +
+    "Tuesday: #{hash[:operatingHours][0][:standardHours][:tuesday]}, " +
+    "Wednesday: #{hash[:operatingHours][0][:standardHours][:wednesday]}, " +
+    "Thursday: #{hash[:operatingHours][0][:standardHours][:thursday]}, " +
+    "Friday: #{hash[:operatingHours][0][:standardHours][:friday]}, " +
+    "Saturday: #{hash[:operatingHours][0][:standardHours][:saturday]}"
   end
 end
